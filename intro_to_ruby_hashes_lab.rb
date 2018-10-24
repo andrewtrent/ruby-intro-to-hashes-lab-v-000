7dc7d2ddeda411f44879ec1c3109022de5606b33
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-24 21:45:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 
 def monopoly
 	monopoly = {:railroads: {}}
-	
+
 end
 
 def monopoly_with_second_tier
@@ -16,7 +16,7 @@ def monopoly_with_second_tier
 		:railroads: {
 			:pieces = "4"
 			}}
-	
+
 end
 
 def monopoly_with_third_tier
